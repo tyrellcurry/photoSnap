a6ab3c025041f3a8cf9fd612ffdfe9a1d83ebfe9
Tyrell C <tyrellrcurry@gmail.com>
2022-05-12 19:41:10 -0700

    Created react app, added: react router, tailwind, tailwind config custom colours, assets, and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      peachOrange: '#FFC593',
+      turkishRose: '#BC7198',
+      dodgerBlue: '#5A77FF',
+      pureBlack: '#000000',
+      lightGrey: '#DFDFDF',
+      pureWhite: 'FFFFFF',
+    },
+  },
+  plugins: [],
+};
