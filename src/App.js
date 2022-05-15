9fb23ba239d9b45e48b3229c57c898c4e9f379db
Rod Garland <rg6797@outlook.com>
2022-05-14 20:14:39 -0400

    Added route views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage';
+import FeaturesPage from './pages/FeaturesPage/FeaturesPage'
+import StoriesPage from './pages/StoriesPage/StoriesPage'
+import PricingPage from './pages/PricingPage/PricingPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-7xl text-pureBlack text-center mt-8 font-bold">
-        PhotoSnap
-      </h1>
+      {/*These are the different views that will be rendered based on the URL Route */}
+      <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/stories' element={<StoriesPage/>}/>
+          <Route path='/features' element={<FeaturesPage/>}/>
+          <Route path='/pricing' element={<PricingPage/>}/>
+          {/*If a route is passed in that is not recognized (for example "/blahblah").. the Not Found Page is displayed*/}
+          <Route path="*" element={<NotFoundPage />} /> {/*  <---- Possible Option: We could redirect the user back to the home page */}
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            Sorry! We cant find that page.....
+        </div>
+    );
+};
+
+export default NotFoundPage;
